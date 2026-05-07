2fde0b458cf4307e93a4bb365c6e48b555ebb4f4
Bharath C Boyapati <bboyapati9@outlook.com>
2026-05-07 15:47:57 +0100

    Updated ReadME

---
+++ DGQ/scripts/quantize_weight.sh
@@ -0,0 +1,44 @@
+# USER CONFIGURATIONS
+export CUDA_VISIBLE_DEVICES=0
+MODEL_TYPE=$1 # sd or sdxl
+W_BITS=$2 # 4 or 8
+echo "MODEL_TYPE: $MODEL_TYPE / W_BITS: $W_BITS"
+
+# CONFIGURATIONS
+export DIFFUSERS_REWRITE=$MODEL_TYPE
+# inference step size # sd: 25 / sdxl: 4
+if [ $MODEL_TYPE = "sd" ]; then
+    STEP_SIZE=25
+    NO_RECON=False # No reconstruction (True: No reconstruction, False: Reconstruction)
+    FAST=False # Weight Initialization with minmax or mse (True: minmax, False: mse)
+elif [ $MODEL_TYPE = "sdxl" ]; then
+    STEP_SIZE=4
+    NO_RECON=True # No reconstruction (True: No reconstruction, False: Reconstruction)
+    FAST=True # Weight Initialization with minmax or mse (True: minmax, False: mse)
+    # Memory and Computation Cost issue
+fi
+CALI_DATA_PATH=./data/cali_data_${MODEL_TYPE}_${STEP_SIZE}steps # calibration data path
+MULTI_GPU=False # Multi-GPU
+TIB_RECON=False # Use TFMQ or Q-Diffusion-> for tib recon
+outdir=results/${MODEL_TYPE}_weight # output path
+COCO_PATH=$3
+
+if [ $MULTI_GPU = True ]; then
+    python src/quantize_weight.py \
+        --wq $W_BITS \
+        --cali_data_path $CALI_DATA_PATH \
+        --step_size $STEP_SIZE \
+        --tib_recon $TIB_RECON \
+        --no_recon $NO_RECON --fast $FAST \
+        --multi_gpu --outdir $outdir \
+        --coco_path $COCO_PATH
+else
+    python src/quantize_weight.py \
+        --wq $W_BITS \
+        --cali_data_path $CALI_DATA_PATH \
+        --step_size $STEP_SIZE \
+        --tib_recon $TIB_RECON \
+        --no_recon $NO_RECON --fast $FAST \
+        --outdir $outdir \
+        --coco_path $COCO_PATH
+fi
